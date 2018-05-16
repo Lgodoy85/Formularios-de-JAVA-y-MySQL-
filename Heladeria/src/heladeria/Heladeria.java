eb1d4101bb63fde0c9e24fe6e9381968e98ebfb6
Lgodoy85 <lgodoy85@gmail.com>
2018-05-16 15:19:34 -0300

    Formulario maximizado

--- Heladeria/src/heladeria/Heladeria.java
+++ Heladeria/src/heladeria/Heladeria.java
@@ -1,21 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package heladeria;
 
-/**
- *
- * @author alumno
- */
+import Vista.FrMPrincipal;
+
 public class Heladeria {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        FrMPrincipal nuevoFormulario= new FrMPrincipal();
+        
+        nuevoFormulario.setExtendedState(nuevoFormulario.MAXIMIZED_BOTH);
+ 
+        nuevoFormulario.setDefaultCloseOperation(nuevoFormulario.EXIT_ON_CLOSE);
+        nuevoFormulario.setVisible(true);
+        
     }
     
 }
